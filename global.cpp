e5d4062d3d9a9a4dc28bcd4696aa0666ad5d7afa
steven-ccq <55176896+steven-ccq@users.noreply.github.com>
2021-11-18 00:07:03 +0800

    Add files via upload

---
+++ global.cpp
@@ -0,0 +1,6 @@
+#include "global.h"
+
+vector<Edge> edges;  //记录所有边
+vector<vector<StarEdgeData>> starEdges;  //记录所有点的星形边
+vector<map<int, vector<int>>> edgeMap;  //记录两点间的所有边
+int nodesNum;  //所有点的个数
