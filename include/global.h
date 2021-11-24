3a5237fd8624f0b17fa8c2f57adff4781b8d2ad3
steven-ccq <55176896+steven-ccq@users.noreply.github.com>
2021-11-24 16:35:39 +0800

    Update global.h

--- include/global.h
+++ include/global.h
@@ -3,10 +3,10 @@
 
 #include "edgeLoader.h"
 
-extern vector<Edge> edges;  //记录所有边
-extern vector<vector<StarEdgeData>> starEdges;  //记录所有点的星形边
-extern vector<map<int, vector<int>>> edgeMap;  //记录两点间的所有边
-extern int nodesNum;  //所有点的个数
+extern vector<Edge> edges;
+extern vector<vector<StarEdgeData>> starEdges;
+extern vector<map<int, vector<int>>> edgeMap;
+extern int nodesNum;
 extern int threadNum;
 extern bool ompFlag;
 extern int thrd;
